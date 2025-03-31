43dbd44bc09deb7224d776952ee592e8e1f41898
matheus-nascimento-dev <sad.ikiba3000@gmail.com>
2025-03-30 22:21:50 -0300

    B: Update with classes and movements done

---
+++ circleshape.py
@@ -0,0 +1,23 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        color = (255,255,255)
+        pygame.draw.polygon(screen, color, self.triangle(), 2)
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
--- main.py
+++ main.py
@@ -3,17 +3,26 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     loop = True
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while loop == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.update()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
