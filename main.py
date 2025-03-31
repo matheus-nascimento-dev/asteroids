72ec61fcb754015d0a1476abeeef08ecde606ab2
matheus-nascimento-dev <sad.ikiba3000@gmail.com>
2025-03-30 22:29:20 -0300

    B: Update with classes and movements done

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ from circleshape import *
 
 def main():
     pygame.init()
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     loop = True
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while loop == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatables.update(dt)
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
+        drawables.draw(screen)
         pygame.display.update()
         clock.tick(60)
         dt = clock.tick(60)/1000
